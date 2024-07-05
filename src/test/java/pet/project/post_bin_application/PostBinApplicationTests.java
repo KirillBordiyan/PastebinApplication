8a4de444dc0cfb9c854b0dd45543c2cf1020052a
Kirill Bordiyan <kirillbordiyan@gmail.com>
2024-07-05 22:00:57 +0300

    init application, add start dependencies, create application.yaml, DB/Minio up in docker, save commands

---
+++ src/test/java/pet/project/post_bin_application/PostBinApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.post_bin_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
