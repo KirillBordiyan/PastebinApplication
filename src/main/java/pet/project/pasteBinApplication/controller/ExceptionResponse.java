8e354e994baa11005dba803bf36c98b085fdf96e
Kirill Bordiyan <kirillbordiyan@gmail.com>
2024-08-15 00:21:02 +0300

    global exception handler are upd (add some meth)

--- src/main/java/pet/project/pasteBinApplication/controller/ExceptionResponse.java
+++ src/main/java/pet/project/pasteBinApplication/controller/ExceptionResponse.java
@@ -1,9 +1,27 @@
 package pet.project.pasteBinApplication.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
 
 @Data
+@AllArgsConstructor
 public class ExceptionResponse {
+
     private int statusCode;
-    private String reason;
+    private String message;
+    private Map<String, String> errors;
+
+    public ExceptionResponse(int statusCode, String message) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+//    public ExceptionResponse(String message) {
+//        this.message = message;
+//    }
+
+
 }
--- src/main/java/pet/project/pasteBinApplication/controller/GlobalExceptionHandler.java
+++ src/main/java/pet/project/pasteBinApplication/controller/GlobalExceptionHandler.java
@@ -1,40 +1,90 @@
 package pet.project.pasteBinApplication.controller;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pet.project.pasteBinApplication.exceptions.AccessDeniedException;
+import pet.project.pasteBinApplication.exceptions.ResourceMappingException;
+import pet.project.pasteBinApplication.exceptions.ResourceNotFoundException;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
-@RestControllerAdvice(basePackageClasses = GlobalExceptionHandler.class)
+@RestControllerAdvice//(basePackageClasses = GlobalExceptionHandler.class)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleException(Exception e){
-        ExceptionResponse response = new ExceptionResponse();
-        response.setStatusCode(500);
-        response.setReason(e.getMessage());
-        return ResponseEntity.internalServerError().body(response);
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handleException(Exception e) {
+        return new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 
+    //TODO этот под вопросом, вроде как его можно заменить IllegalStateExc
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ExceptionResponse> handleIllegalArgument(NoSuchElementException e){
-        ExceptionResponse response = new ExceptionResponse();
-        response.setStatusCode(404);
-        response.setReason(e.getMessage());
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleIllegalArgument(NoSuchElementException e) {
+        return new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<ExceptionResponse> handleIOException(IOException e){
-        ExceptionResponse response = new ExceptionResponse();
-        response.setStatusCode(406);
-        response.setReason(e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public ExceptionResponse handleIOException(IOException e) {
+        return new ExceptionResponse(HttpStatus.NOT_ACCEPTABLE.value(), e.getMessage());
     }
 
-    //TODO пополнять обработкой любого исключения, которое происходит в REST-контроллерах
-    // не забыть переместить этот хендлер в папку с рестКонтроллерами
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleIllegalStateException(IllegalStateException e) {
+        return new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse handleResourceNotFound(ResourceNotFoundException e) {
+        return new ExceptionResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceMappingException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handleResourceMapping(ResourceMappingException e) {
+        return new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
+
+    @ExceptionHandler({AccessDeniedException.class, org.springframework.security.access.AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionResponse handleAccessDenied() {
+        return new ExceptionResponse(HttpStatus.FORBIDDEN.value(), "Access denied!");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_GATEWAY.value(), "Some validation fail!");
+        List<FieldError> errorsList = e.getBindingResult().getFieldErrors();
+        response.setErrors(errorsList.stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
+        return response;
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleConstraintViolation(ConstraintViolationException e) {
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), "Some validation fail!");
+        response.setErrors(e.getConstraintViolations()
+                .stream()
+                .collect(Collectors.toMap(
+                        violation -> violation.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage
+                )));
+
+        return response;
+    }
 }
